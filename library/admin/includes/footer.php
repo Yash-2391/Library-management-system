88648b4f5353f327bdbce65392b4aa98b5815577
yash gohil <yashgohil2391@gmail.com>
2023-03-20 19:15:39 +0530

    first commit

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   SoundCoder&copy; Library Management System | CreaterID : 1111617 | version 1.0.0 .
+                </div>
+
+            </div>
+        </div>
+    </section>
